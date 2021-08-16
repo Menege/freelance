2c4c373de8f56976bf269f6d124e16e76cc4528f
user <it.efrem94@yandex.ru>
2021-08-16 12:31:47 +0300

    add route link

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="navbar">
+    <h3 class="">Freelance</h3>
+    <ul class="navbar-menu">
+      <li>
+        <router-link to="/tasks">Все задачи</router-link>
+      </li>
+      <li>
+        <router-link to="/new">Создать</router-link>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
